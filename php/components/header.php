f626b4b3e0ea14ad5e25cb21f06dcc73e2fc2676
Louismwebg <guillotmartinlouis@gmail.com>
2026-01-12 17:40:39 +0100

    Header + footer + css

---
+++ php/components/header.php
@@ -0,0 +1,35 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>US Colomiers - SAE 301</title>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=PT+Sans+Narrow:wght@400;700&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<header class="main-header">
+    <div class="header-container">
+        <div class="header-left">
+            <a href="index.php" class="logo-link">
+                <img src="../../assets/Logo_Colomiers_Football.svg" alt="Logo US Colomiers" class="main-logo">
+            </a>
+            <h1 class="site-title">US COLOMIERS</h1>
+        </div>
+
+        <nav class="nav-menu">
+            <ul>
+                <li><a href="#">Le Club</a></li>
+                <li><a href="#">National 3</a></li>
+                <li><a href="#">Autres équipes</a></li>
+                <li><a href="#">Actualités</a></li>
+            </ul>
+        </nav>
+
+        <div class="header-right">
+            <a href="#" class="btn-boutique">BOUTIQUE</a>
+        </div>
+    </div>
+</header>
