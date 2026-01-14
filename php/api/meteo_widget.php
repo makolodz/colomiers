1705f0bb16dfa38870ddae29868adc59b8d54c79
colomiers Kylian Bidault <colomiers Kylian.BIDAULT@etu.uca.fr>
2026-01-14 14:05:46 +0100

    Fonctionnalité API Météo + .gitignore

---
+++ php/api/meteo_widget.php
@@ -0,0 +1,15 @@
+<?php include __DIR__ . '/meteo.php'; ?>
+<link rel="stylesheet" href="./css/meteo.css">
+<div class="meteo-widget">
+    <strong>Météo - <?php echo ($ville) ?></strong>
+    <div class="meteo-days">
+        <?php foreach (array_slice($previsions, 0, 3) as $jour): ?>
+            <div class="meteo-day">
+                <div><?= date('d/m', strtotime($jour['date'])) ?></div>
+                <img src="https://openweathermap.org/img/wn/<?= $jour['icon'] ?>.png" alt="">
+                <div><?= $jour['temp'] ?>°C</div>
+                <small><?php($jour['desc']) ?></small>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
