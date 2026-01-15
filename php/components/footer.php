ba52c440f4feb4e85ea106cce23b5e2c58ce66c7
LucasMunoz105 <Lucas.MUNOZ@etu.uca.fr>
2026-01-15 10:35:07 +0100

    FOOTER SOCIAL LINKS
    insta + facebook

--- php/components/footer.php
+++ php/components/footer.php
@@ -11,8 +11,17 @@
 
         <div class="footer-right">
             <div class="social-links">
+<<<<<<< Updated upstream
                 <a href="#" class="social-box" aria-label="Facebook"></a>
                 <a href="#" class="social-box" aria-label="Instagram"></a>
+=======
+              <a href="https://www.facebook.com/us.colomiers.7/?locale=fr_FR" class="social-box">
+               <img src="./assets/facebook.png" alt="Facebook">
+                </a>
+              <a href="https://www.instagram.com/uscolomiersfootball/?hl=fr" class="social-box">
+               <img src="./assets/instagram.webp" alt="Instagram">
+                </a>
+>>>>>>> Stashed changes
             </div>
             <a href="#" class="btn-main btn-small">Boutique</a>
         </div>
--- php/components/header.php
+++ php/components/header.php
@@ -9,10 +9,17 @@
 
         <nav class="nav-menu">
             <ul>
+<<<<<<< Updated upstream
                 <li><a href="club.php">Le Club</a></li>
                 <li><a href="national3.php">National 3</a></li>
                 <li><a href="autresequipes.php">Autres équipes</a></li>
                 <li><a href="actualites.php">Actualités</a></li>
+=======
+                <li><a href="./club.php">Le Club</a></li>
+                <li><a href="./national3.php">National 3</a></li>
+                <li><a href="./autresequipes.php">Autres équipes</a></li>
+                <li><a href="./actualites.php">Actualités</a></li>
+>>>>>>> Stashed changes
             </ul>
         </nav>
 
